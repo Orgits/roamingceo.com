159ccbf14bd05679e9849c1972a8c9419445d311
Anubhav Sharma <anubhav@Anubhavs-MacBook-Air.local>
2025-02-02 22:35:33 +0530

    ok

--- components/elements/backToTop.js
+++ components/elements/backToTop.js
@@ -1,11 +1,5 @@
-import ScrollToTop from "react-scroll-up";
+import ScrollToTop from "react-scroll-to-top";
 
 export default function BackToTop() {
-    return (
-        <ScrollToTop showUnder={160}>
-            <a id="scrollUp" style={{ display: "block" }}>
-                <i className="elegant-icon arrow_up"></i>
-            </a>
-        </ScrollToTop>
-    );
+  return <ScrollToTop smooth />;
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,42 +1,50 @@
-import React from "react";
-React.useLayoutEffect = React.useEffect;
-import { useEffect } from "react";
-import { useRouter } from 'next/router'
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 
-import 'react-perfect-scrollbar/dist/css/styles.css';
+import "react-perfect-scrollbar/dist/css/styles.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../public/assets/css/style.css";
 import "../public/assets/css/widgets.css";
 import "../public/assets/css/responsive.css";
-import 'metismenujs/dist/metismenujs.css';
+
+// Ensure MetisMenu CSS is properly imported if available
+try {
+  require("metismenujs/dist/metismenujs.css");
+} catch (error) {
+  console.warn("MetisMenu CSS not found, skipping import.");
+}
 
 function MyApp({ Component, pageProps }) {
-    const router = useRouter()
+  const router = useRouter();
 
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            window.WOW = require("wowjs");
-        }
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const WOW = require("wowjs");
+      new WOW.WOW().init();
+    }
 
-        new WOW.WOW().init();
+    const gridElement = document.querySelector(".grid-sizer");
+    if (gridElement) {
+      const Masonry = require("masonry-layout");
+      new Masonry(".grid", {
+        itemSelector: ".grid-item",
+        columnWidth: ".grid-sizer",
+      });
+    }
 
-        const hasGridClass = document.querySelector('.grid-sizer');
-        
-        if (hasGridClass != null) {
-            const xyz = require("masonry-layout");
-            var msnry = new xyz(".grid", {
-                itemSelector: ".grid-item",
-                columnWidth: ".grid-sizer"
-            });
-        }    
-        
-        return () => {
-            router.events.off('routeChangeError', handleRouteChangeError)
-          }
-    }, []);
-    return <Component {...pageProps} />;
-    
+    const handleRouteChangeError = () => {
+      console.error("Route change error occurred.");
+    };
+
+    router.events.on("routeChangeError", handleRouteChangeError);
+
+    return () => {
+      router.events.off("routeChangeError", handleRouteChangeError);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
